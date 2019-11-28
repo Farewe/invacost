4cf8accba18fabc6595031a6267ed8796b8bcc0e
Farewe <leroy.boris@gmail.com>
2019-11-28 13:11:45 +0100

    Functional version of the package, updated description and added/finished generic functions

--- R/genericfunctions.R
+++ R/genericfunctions.R
@@ -3,12 +3,40 @@
 print.invacost.annual.est <- function(x, ...)
 {
   cat("Estimation of annual cost values of invasive alien species over time\n")
-  cat(paste0("- Temporal interval of data : [", 
-             min(x$cost.data$Year, na.rm = TRUE),
+  cat(paste0("\n- Temporal interval of data : [", 
+             x$parameters$minimum.year,
              ", ",
-             max(x$cost.data$Year, na.rm = TRUE), "]"))
-  cat(paste0("- Temporal interval used for model calibration: ", 
-             min(x$cost.data$Year, na.rm = TRUE)))
+             x$parameters$maximum.year, "]"))
+  cat(paste0("\n- Temporal interval used for model calibration: [", 
+             x$parameters$minimum.year,
+             ", ",
+             x$parameters$incomplete.year.threshold, "]"))
+  cat(paste0("\n- Cost transformation: ", 
+             x$parameters$cost.transformation))
+  cat(paste0("\n- Values transformed in US$ million: ", 
+             ifelse(x$parameters$in.millions, "Yes", "No")))
+  cat(paste0("\n- Estimated average annual cost of invasive alien species in ",
+             x$parameters$final.year, ":\n",
+             "\n   o Linear regression: US$ ",
+             ifelse(x$parameters$in.millions, "million ", ""),
+             scales::comma(x$final.year.cost["linear"]),
+             "\n   o Multiple Adapative Regresssion Splines: US$ ",
+             ifelse(x$parameters$in.millions, "million ", ""),
+             scales::comma(x$final.year.cost["mars"]),
+             "\n   o Generalized Additive Model: US$ ",
+             ifelse(x$parameters$in.millions, "million ", ""),
+             scales::comma(x$final.year.cost["gam"]),
+             "\n   o Quantile regression: ",
+             "\n     . Quantile 0.1: US$ ", 
+             ifelse(x$parameters$in.millions, "million ", ""),
+             scales::comma(x$final.year.cost["quantile.0.1"]),
+             "\n     . Quantile 0.5: US$ ", 
+             ifelse(x$parameters$in.millions, "million ", ""),
+             scales::comma(x$final.year.cost["quantile.0.5"]),
+             "\n     . Quantile 0.9: US$ ", 
+             ifelse(x$parameters$in.millions, "million ", ""),
+             scales::comma(x$final.year.cost["quantile.0.9"])
+             ))
 }
 
 #' @export
@@ -30,64 +58,3 @@ plot.invacost.annual.est <- function(x, ...)
   print(x$plot)
 }
 
-#' @export
-#' @method print VSSampledPoints
-print.VSSampledPoints <- function(x, ...)
-{
-  # Next line is to ensure retrocompatibility with earlier versions of
-  # virtualspecies where no print function was designed for VSSampledPoints
-  if(!is.list(x$detection.probability))
-  {
-    print(x)
-  } else
-  {
-    cat(paste("Occurrence points sampled from a virtual species"))
-    cat(paste("\n\n- Type:", x$type))
-    cat(paste("\n- Number of points:", nrow(x$sample.points)))
-    if(length(x$bias))
-    {
-      cat("\n- Sampling bias: ")
-      cat(paste("\n   .Bias type:", 
-                x$bias$bias))
-      cat(paste("\n   .Bias strength:",
-                x$bias$bias.strength))
-    } else
-    {
-      cat("\n- No sampling bias")
-    }
-    cat(paste0("\n- Detection probability: "))
-    cat(paste0("\n   .Probability: ", x$detection.probability$detection.probability))
-    cat(paste0("\n   .Corrected by suitability: ", x$detection.probability$correct.by.suitability))
-    cat(paste0("\n- Probability of identification error (false positive): ", x$error.probability))
-    if(length(x$sample.prevalence))
-    {
-      cat(paste0("\n- Sample prevalence: "))
-      cat(paste0("\n   .True:", x$sample.prevalence["true.sample.prevalence"]))
-      cat(paste0("\n   .Observed:", x$sample.prevalence["observed.sample.prevalence"]))
-    }
-    cat(paste0("\n- Multiple samples can occur in a single cell: ", 
-               ifelse(x$replacement, "Yes", "No")))
-    cat("\n\n")
-    if(nrow(x$sample.points) > 10)
-    {
-      cat("First 10 lines: \n")
-      print(x$sample.points[1:10, ])
-      cat(paste0("... ", nrow(x$sample.points) - 10, " more lines.\n"))
-    } else
-    {
-      print(x$sample.points)
-    }
-  }
-}
-
-#' @export
-#' @method str VSSampledPoints
-str.VSSampledPoints <- function(object, ...)
-{
-  args <- list(...)
-  if(is.null(args$max.level))
-  {
-    args$max.level <- 2
-  }
-  NextMethod("str", object = object, max.level = args$max.level)
-}

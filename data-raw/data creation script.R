210fdeaf268bab623e7a663aaf5d145f80c98d38
unknown <leroy.boris@gmail.com>
2020-08-04 14:56:44 +0200

    Correction to erroneous entry Reference_ID 8733 in INVACOST

--- data-raw/data creation script.R
+++ data-raw/data creation script.R
@@ -20,22 +20,11 @@ invacost$Version[grep("NE", invacost$Cost_ID)] <- "V2"
 invacost$Version[grep("SC", invacost$Cost_ID)] <- "V2-1"
 
 
-# invacost <- invacost[-which(is.na(invacost$Annualised_cost_estimate_2017_USD_exchange_rate)), ]
-# invacost <- invacost[-which(invacost$`To remove` == "yes"), ]
-# invacost[which(invacost$Reference_ID == 8733 & 
-#                  invacost$Repository == "WoS"), "Method_reliability"] <- "Low"
-# invacost[which(invacost$Cost_ID == 1386), "Method_reliability"] <- "Low"
-# invacost[which(invacost$Cost_ID == 1175), "Method_reliability"] <- "Low"
-# 
-# invacost <- invacost[, -which(
-#   colnames(invacost) %in% c("Benefit_value(s)",
-#                             "Probable_starting_year_high_margin",
-#                             "Probable_ending_year_high_margin",
-#                             "Impacted_sector_2",
-#                             "To.remove",
-#                             "Type_2",
-#                             "Spatial_scale2",
-#                             "To remove"))]
+
+# We remove this line until it is officially excluded from INVACOST as, after checking,
+# it is not adequate (budget corresponds to both native & non native species management with no distinction)
+invacost <- invacost[-which(invacost$Reference_ID == 8733 & 
+                              invacost$Repository == "WoS"), ]
 
 usethis::use_data(invacost, overwrite = TRUE)
 
